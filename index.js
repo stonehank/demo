a4de52486fe3235b1c67d3e3983efd2753a924be
hank <stonehank310@gmail.com>
2018-08-19 01:26:02 +0800

    增加xhr.progress，修复最后会多传数据bug

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ let oProgress = document.getElementById("progress")
 let progressNum=document.getElementById("progressNum")
 
 let allSize
+// 数据分割值
 let gap = 1024*1024
+// 剩下的数据大小
+let lastSize
+// 当前次上传数据值（如果剩下的小于分割值，就取剩下的，否则去分割值）
+let curUpload
 let progress = 0
 let progressW = parseInt(oProgress.clientWidth, 10)
-let start = 0, end = start + gap;
+let start = 0, end
 let isUpLoading = false
 let blob
 let xhr = new XMLHttpRequest()
@@ -24,11 +29,15 @@ control.addEventListener('click',function () {
     isUpLoading=true
     blob = file.files[0];
     allSize = blob.size;
+    lastSize=allSize
+    curUpload=lastSize-gap>0?gap:lastSize
+    end=start+curUpload
     segmentUpload(start,end)
+    control.innerText=isUpLoading?"暂停":"继续"
   }else{
     isUpLoading=false;
   }
-  control.innerText=isUpLoading?"暂停":"继续"
+
 })
 
 file.addEventListener('change',function () {
@@ -42,25 +51,47 @@ cancel.addEventListener('click',function () {
 })
 
 function segmentUpload(start, end) {
-  if (start > allSize) return
+  console.log(start,end,allSize)
+  if (start >= allSize) return
   let fd = new FormData()
   let cur = blob.slice(start, end, blob.type)
   fd.append('name', blob.name)
   fd.append('start', start)
   fd.append('fragments', cur);
   xhr.open('POST', 'http://localhost:8080/upload.js', true);
+  xhr.upload.onprogress=function(progress){
+    if (progress.lengthComputable ) {
+      progressUpdate(progress.loaded / progress.total)
+    }
+  }
   xhr.send(fd)
 }
-
-function done(progress) {
-  if (progress >= 1) {
+function progressUpdate(addSize){
+  if(!isUpLoading){
+    control.disabled=true
+    control.innerText="请稍后..."
+    if(addSize===1){
+      control.disabled=false
+      control.innerText=isUpLoading?"暂停":"继续"
+      return
+    }
+  }
+  let newProgress=0
+  if(addSize){
+    newProgress=addSize*(curUpload/allSize>1?1:curUpload/allSize)
+  }
+  if (progress+newProgress >= 1) {
     progress = 1;
+    newProgress=0
     file.value=''
     dataClear()
   }
-  progressNum.innerText=(progress*100).toFixed(2)+"%";
-  progressColor.style.transform = `translate(${progressW * (progress - 1)}px,0)`
+  progressNum.innerText=((progress+newProgress)*100).toFixed(2)+"%";
+  progressColor.style.transform = `translate(${progressW * (progress+newProgress - 1)}px,0)`
 }
+
+
+
 function dataClear(){
   isUpLoading=false
   start = 0;
@@ -83,11 +114,16 @@ xhr.onreadystatechange = function () {
         clear()
       } else {
         progress = end / allSize
+        lastSize=allSize-end
+        curUpload=lastSize-gap>0?gap:lastSize
+        // console.log(progress,lastSize)
         start = end
-        end = start + gap
+        end = start + curUpload
+        // console.log('new',start,end)
+        // 只有开始传送的时候才会更新
         if(isUpLoading){
-          done(progress)
           segmentUpload(start, end)
+          progressUpdate()
         }
       }
     }
